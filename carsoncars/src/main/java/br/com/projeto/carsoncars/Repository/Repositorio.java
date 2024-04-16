be5dafc636e6b8f54adf85a39fa752d8ed82f415
flepes <felgom@alunos.utfpr.edu.br>
2024-04-15 22:05:41 -0300

    Git reset
    git reset

---
+++ carsoncars/src/main/java/br/com/projeto/carsoncars/Repository/Repositorio.java
@@ -0,0 +1,17 @@
+package br.com.projeto.carsoncars.Repository;
+
+import java.util.List;
+import java.util.Optional;
+//import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.projeto.carsoncars.Entities.User.User;
+
+@Repository
+public interface Repositorio extends CrudRepository<User, UUID> {
+    List<User> findAll();
+    Optional<User> findById(UUID id); // Renomeando o método para evitar conflito
+    User findByEmail(String email);
+}
---
+++ src/main/java/br/com/projeto/carsoncars/CarsoncarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.carsoncars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsoncarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsoncarsApplication.class, args);
+	}
+
+}
