2f7a5bd4fbb506b9c927667a1cdac454cbdb2013
flepes <felgom@alunos.utfpr.edu.br>
2024-05-11 17:04:02 -0300

    Add Criptografia e JWS

--- src/main/java/br/com/projeto/carsoncars/CarsoncarsApplication.java
+++ src/main/java/br/com/projeto/carsoncars/CarsoncarsApplication.java
@@ -2,12 +2,20 @@ package br.com.projeto.carsoncars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
-public class CarsoncarsApplication {
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class CarsoncarsApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarsoncarsApplication.class, args);
 	}
-
+	@Bean
+	public PasswordEncoder getPasswordEncoder(){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+	}
 }
